f0ff95aacf082e7f8e2168e3637c5f7494cf263f
dev-uros <uros.minic@kentkart.rs>
2024-02-04 18:54:21 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,124 @@
+<script setup>
+import { copyToClipboard } from 'quasar'
+
+const clipboard = text => {
+  copyToClipboard(text)
+}
+console.log('👋 This message is being logged by "App.vue", included via Vite')
+</script>
+
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page padding>
+        <q-list padding class="rounded-borders">
+          <q-expansion-item
+            dense
+            dense-toggle
+            expand-separator
+            icon="dns"
+            label="206.32.91"
+          >
+            <q-list dense>
+              <q-item>
+                <q-item-section>
+                  <q-item-label>206.32.91</q-item-label>
+                  <q-item-label caption>Ip</q-item-label>
+                </q-item-section>
+                <q-item-section avatar>
+                  <q-btn
+                    flat
+                    icon="content_copy"
+                    size="sm"
+                    @click="clipboard('127.01.01')"
+                  ></q-btn>
+                </q-item-section>
+              </q-item>
+              <q-item>
+                <q-item-section>
+                  <q-item-label>Bobasmrad2@</q-item-label>
+                  <q-item-label caption>Password</q-item-label>
+                </q-item-section>
+                <q-item-section avatar>
+                  <q-btn
+                    flat
+                    icon="content_copy"
+                    size="sm"
+                    @click="clipboard('Bobasmrad2@')"
+                  ></q-btn>
+                </q-item-section>
+              </q-item>
+              <q-item>
+                <q-item-section>
+                  <q-item-label>/var/www/html</q-item-label>
+                  <q-item-label caption>Projects root directory</q-item-label>
+                </q-item-section>
+                <q-item-section avatar>
+                  <q-btn
+                    flat
+                    icon="content_copy"
+                    size="sm"
+                    @click="clipboard('/var/www/html')"
+                  ></q-btn>
+                </q-item-section>
+              </q-item>
+            </q-list>
+            <q-timeline color="secondary" style="padding: 15px">
+              <q-timeline-entry title="Komunalna milicija" icon="local_police">
+                <p class="text-body2 text-weight-light">
+                  Backoffice application for communal police of Belgrade
+                </p>
+                <p class="text-body2 text-weight-light">
+                  Root: /var/www/html/communal-police-backoffice
+                </p>
+              </q-timeline-entry>
+
+              <q-timeline-entry title="A-voucher" icon="redeem">
+                <p class="text-body2 text-weight-light">
+                  Backoffice application for A-vocher topup service
+                </p>
+                <p class="text-body2 text-weight-light">
+                  Root: /var/www/html/a-voucher
+                </p>
+              </q-timeline-entry>
+            </q-timeline>
+          </q-expansion-item>
+        </q-list>
+        <q-page-sticky position="bottom-right" :offset="[18, 18]">
+          <q-fab padding="5px" icon="unfold_more" direction="up" color="accent">
+            <q-fab-action
+              padding="2px"
+              @click="onClick"
+              color="primary"
+              icon="add_circle"
+            >
+              <q-tooltip
+                class="bg-primary"
+                anchor="center left"
+                self="center right"
+                :offset="[10, 10]"
+              >
+                Kreiraj
+              </q-tooltip></q-fab-action
+            >
+            <q-fab-action
+              padding="2px"
+              @click="onClick"
+              color="primary"
+              icon="download"
+            >
+              <q-tooltip
+                class="bg-positive"
+                anchor="center left"
+                self="center right"
+                :offset="[10, 10]"
+              >
+                Exportuj
+              </q-tooltip></q-fab-action
+            >
+          </q-fab>
+        </q-page-sticky>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
---
+++ src/renderer.ts
@@ -0,0 +1,76 @@
+/**
+ * This file will automatically be loaded by vite and run in the "renderer" context.
+ * To learn more about the differences between the "main" and the "renderer" context in
+ * Electron, visit:
+ *
+ * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
+ *
+ * By default, Node.js integration in this file is disabled. When enabling Node.js integration
+ * in a renderer process, please be aware of potential security implications. You can read
+ * more about security risks here:
+ *
+ * https://electronjs.org/docs/tutorial/security
+ *
+ * To enable Node.js integration in this file, open up `main.ts` and enable the `nodeIntegration`
+ * flag:
+ *
+ * ```
+ *  // Create the browser window.
+ *  mainWindow = new BrowserWindow({
+ *    width: 800,
+ *    height: 600,
+ *    webPreferences: {
+ *      nodeIntegration: true
+ *    }
+ *  });
+ * ```
+ */
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+// console.log(
+//   '👋 This message is being logged by "renderer.ts", included via Vite'
+// )
+
+// FILE: main.js
+
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+
+// A few examples for animations from Animate.css:
+// import @quasar/extras/animate/fadeIn.css
+// import @quasar/extras/animate/fadeOut.css
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
+import App from './App.vue'
+
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+  /*
+  config: {
+    brand: {
+      // primary: '#e46262',
+      // ... or all other brand colors
+    },
+    notify: {...}, // default set of options for Notify Quasar plugin
+    loading: {...}, // default set of options for Loading Quasar plugin
+    loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+    // ..and many more (check Installation card on each Quasar component/directive/plugin)
+  }
+  */
+})
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app')
