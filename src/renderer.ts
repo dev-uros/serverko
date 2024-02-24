cca832a282380483a4e527b9ecdf30b1eb6e319b
dev-uros <uros.minic@kentkart.rs>
2024-02-24 22:58:40 +0100

    Test message in comments

--- src/renderer.ts
+++ src/renderer.ts
@@ -50,6 +50,7 @@ import '@quasar/extras/material-icons/material-icons.css'
 // Import Quasar css
 import 'quasar/src/css/index.sass'
 
+//test message
 // Assumes your root component is App.vue
 // and placed in same folder as main.js
 import App from './App.vue'
